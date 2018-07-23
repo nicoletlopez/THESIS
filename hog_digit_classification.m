322215be28feeb637a87a87df7bddfb97a4bd546
Justin Parado <japarado@student.apc.edu.ph>
2018-07-23 18:43:48 +0800

    Added image preprocessing stage. This is not Sauvola's yet

--- hog_digit_classification.m
+++ hog_digit_classification.m
@@ -14,3 +14,36 @@ testSet     = imageDatastore(handwrittenDir, 'IncludeSubfolders', true, 'LabelSo
 %count of each class sample
 countEachLabel(trainingSet)
 countEachLabel(testSet)
+
+%show some of the training set
+figure;
+
+subplot(2,3,1);
+imshow(trainingSet.Files{102});
+
+subplot(2,3,2);
+imshow(trainingSet.Files{304});
+
+subplot(2,3,3);
+imshow(trainingSet.Files{809});
+
+subplot(2,3,4);
+imshow(testSet.Files{13});
+
+subplot(2,3,5);
+imshow(testSet.Files{37});
+
+subplot(2,3,6);
+imshow(testSet.Files{97});
+
+% Show pre-processing results
+exTestImage = readimage(testSet,37);
+processedImage = imbinarize(rgb2gray(exTestImage));
+
+figure;
+
+subplot(1,2,1)
+imshow(exTestImage)
+
+subplot(1,2,2)
+imshow(processedImage)
