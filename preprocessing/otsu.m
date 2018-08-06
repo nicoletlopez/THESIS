20984c963d303d72ab5a6fbac53e76115fa75d13
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-08-06 22:48:34 +0800

    Deleted data sets

--- preprocessing/otsu.m
+++
@@ -1,8 +0,0 @@
-clc; clear;
-
-image = imread('check_no_edge.jpg');
-level = graythresh(image);
-otsu_bin = imbinarize(image,level);
-
-% imshowpair(image,otsu_bin,'montage');
-imshowpair(image,otsu_bin,'montage');
