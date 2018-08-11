b118fa48ed45a6e42c93a7d239103331f85a07aa
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-08-11 17:49:38 +0800

    added displays

--- misc/preprocess_then_save.m
+++ misc/preprocess_then_save.m
@@ -1,5 +1,6 @@
 clc;clear;close all
 
+disp('----------------START----------------');
 [file,path,indx]=uigetfile('*.png','Select Images','MultiSelect','on');
 fileCount=numel(cellstr(file));
 
@@ -9,6 +10,7 @@ filePath=fullfile(path,file);
 strFileCount=int2str(fileCount);
 folderName=strcat(strFileCount,string(file(1)),int2str(mod(fileCount,2)),string(file(fileCount)));
 mkdir(folderName);
+disp('Pre-processing and saving images...');
 for i=1:fileCount
     filePath=cellstr(filePath);
     
@@ -28,6 +30,8 @@ for i=1:fileCount
     
     fileName=sprintf('preprocessed-image%d.png',i);
     fullFileName=fullfile(char(folderName),char(fileName));
-    imwrite(im,fullFileName);
+    
+    imwrite(im,fullFileName,'png');
     %export_fig(fullFileName,'png')
 end
+disp('----------------END----------------');
