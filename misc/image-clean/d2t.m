918f0a0f96b180b9c10ec1cdab2ed74dc88defeb
Nicole Angelyn T. Lopez <nicoleeduworklopez@gmail.com>
2018-08-16 20:17:43 +0800

    Clean data function

---
+++ misc/image-clean/d2t.m
@@ -0,0 +1,18 @@
+clc;
+
+categories = {'0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'A', 'b', 'B', 'C', ...
+    'd', 'D', 'e', 'E', 'f', 'F', 'g', 'G', 'h', 'H', 'I', 'J', 'K', 'L', ...
+    'M', 'n', 'N', 'O', 'P', 'q', 'Q', 'r', 'R', 'S', 't', 'T', 'U', ...
+    'V', 'W', 'X', 'Y', 'Z'};
+
+
+disp('START...');
+for ctr = 1:numel(categories)
+    %change this
+    directory_string = '/home/nicole/Documents/FINAL/clean-data/';
+    directory_string = string(strcat(directory_string,categories(ctr)));
+    dirty2tidy(directory_string);
+    disp([num2str(ctr), ' - ', categories(ctr)])
+end
+disp('END...');
+
---
+++ misc/image-clean/dirty2tidy.m
@@ -0,0 +1,40 @@
+function dirty2tidy(filename)
+%Get image names
+if isstruct(filename) %assuming it is a struct as produced from DIR
+    files=filename;
+elseif isdir(filename) %if the input is a directory, get all the image files from the directory
+    currentdir=pwd;
+    cd (filename)
+    files=[dir('*.png'); dir('*.gif'); dir('*.bmp'); dir('*.jpg')];
+else %if it is a single file:
+    files.name=filename;
+end
+%loop over all the files
+for n=1:length(files)
+    filename=files(n).name;
+    [im,map]=imread(filename);
+    im16=im2uint16(im);
+    if(isempty(map))
+        if(size(im16,3)==1)
+            bin=imbinarize(im16);
+        else
+            gray=rgb2gray(im16);
+            bin=imbinarize(gray);
+        end
+    end
+    bw=imcomplement(bin);
+    blobAnalyzer=vision.BlobAnalysis('MaximumCount',10);
+    [area, ~, roi] = step(blobAnalyzer, bw);
+    areaConstraint=area<max(area);
+    roi = double(roi(areaConstraint, :));
+    im=insertShape(im,'filledrectangle',roi,'color','white','opacity',1);
+    im=rgb2gray(im);
+    im=im2uint16(im);
+    im = repmat(im,[1,1,3]);
+    im=uint8(im);
+    imwrite(im,filename)
+end
+%change back to calling directory, if necessary
+if exist('currentdir','var')
+    cd(currentdir)
+end
