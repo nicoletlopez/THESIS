607dd62732cebe81c005bb7fbdf0b6f4668d5c48
Jade Ericson Adriano <jmadriano2@student.apc.edu.ph>
2018-08-22 02:07:00 +0800

    Fixed Test Feature Parameters

--- classifiers/Pro Method/FeatureClass.m
+++ classifiers/Pro Method/FeatureClass.m
@@ -24,13 +24,17 @@ classdef FeatureClass < handle
         function generateTrainingFeatures(obj)
             disp('Generating TRAINING FEATURES...');
             tic;
+            disp('Creating Training Image Data Store...');
             trainingDatastore = obj.imageSetClass.trainingDatastore;
+            disp('Getting Training Image Data Store Size...');
             inputSize = obj.net.Layers(1).InputSize;
+            disp('Augmenting Training Image Data Store...');
             augmentedImages = augmentedImageDatastore(inputSize,trainingDatastore,...
                 'ColorPreprocessing','gray2rgb');
+            disp('Extracting Training Features...');
             obj.trainingFeatures = activations(obj.net,augmentedImages,...
-            obj.featureLayer,'MiniBatchSize',32,'OutputAs','columns',...
-            'ExecutionEnvironment','gpu');
+                obj.featureLayer,'MiniBatchSize',32,'OutputAs','columns',...
+                'ExecutionEnvironment','gpu');
             obj.trainingFeaturesExtractionTime = toc;
             disp('TRAINING FEATURES generated');
 
@@ -41,12 +45,17 @@ classdef FeatureClass < handle
         function generateTestFeatures(obj)
             disp('Generating TEST FEATURES...');
             tic;
+            disp('Creating Testing Image Data Store...');
             testDatastore = obj.imageSetClass.testDatastore;
+            disp('Getting Testing Image Data Store Size...');
             inputSize = obj.net.Layers(1).InputSize;
+            disp('Augmenting Testing Image Data Store...');
             augmentedImages = augmentedImageDatastore(inputSize,testDatastore,...
                 'ColorPreprocessing','gray2rgb');
+            disp('Extracting Testing Features...');
             obj.testFeatures = activations(obj.net,augmentedImages,...
-                obj.featureLayer,'ExecutionEnvironment','gpu');
+                obj.featureLayer,'MiniBatchSize',32,'OutputAs','columns',...
+                'ExecutionEnvironment','gpu');
             disp('TEST FEATURES generated');
             obj.testFeaturesExtractionTime = toc;
 
